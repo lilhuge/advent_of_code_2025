f38d00dab1cf87dacc5c3bd1612d328619d1d231
Hugo Matthews <hugo.matthews@outlook.com>
2025-12-02 20:24:11 +0000

    chore: add comments

--- day02/solve.py
+++ day02/solve.py
@@ -41,20 +41,23 @@ def part2(number_sets):
             string_len = len(num_str)
 
             
-            # Get the largest integer repeatable pattern
+            # Get the largest integer repeatable pattern for the string length
             longest_repeatable_length = len(num_str) // 2
+
 
             for pattern_len in range(1, longest_repeatable_length +1):
                 # If string length isn't divisible by pattern length, skip 
                 if string_len % pattern_len !=0:
                     continue
 
+                # Split the sting into even chunks of length = pattern_len
                 pattern_chunks = [num_str[i:i + pattern_len] for i in range(0, string_len, pattern_len)]
 
                 all_chunks_equal = len(set(pattern_chunks)) == 1
 
                 if all_chunks_equal:
                     invalid_sum += number
+                    # If matched for a given pattern length, break so we don't count more than once
                     break
 
 
